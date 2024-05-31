458e4c1b7c848449ab14e4f87bcdec4bf26336e9
96ayad <167616524+96ayad@users.noreply.github.com>
2024-05-31 21:26:50 +0300

    action

--- src/components/token/CreateToken2.tsx
+++ src/components/token/CreateToken2.tsx
@@ -156,7 +156,7 @@ export const CreateToken2: FC = () => {
         </h3>
       </div>
       <div className="p-7">
-        <form onSubmit={() => onClick({decimals: Number(decimals), amount: Number(amount), metadata: metadata, symbol: symbol, tokenName: tokenName})}>
+        <form action="#">
 
 
           <div className="mb-5.5">
@@ -307,7 +307,7 @@ export const CreateToken2: FC = () => {
             <button
               className="flex justify-center rounded bg-primary px-6 py-2 font-medium text-gray hover:bg-opacity-90"
               type="submit"
-            //   onClick={() => onClick({decimals: Number(decimals), amount: Number(amount), metadata: metadata, symbol: symbol, tokenName: tokenName})}
+              onClick={() => onClick({decimals: Number(decimals), amount: Number(amount), metadata: metadata, symbol: symbol, tokenName: tokenName})}
             >
               create
             </button>
